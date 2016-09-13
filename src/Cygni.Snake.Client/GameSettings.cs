154d2b349befab4879dfc7306d5288e0c8a43c65
Daniel Lenksjö <dlenksjo@gmail.com>
2016-09-13 18:56:33 +0200

    Remove obsolete game settings
    This closes #2

--- src/Cygni.Snake.Client/GameSettings.cs
+++ src/Cygni.Snake.Client/GameSettings.cs
@@ -5,18 +5,6 @@ namespace Cygni.Snake.Client
     public class GameSettings
     {
         /// <summary>
-        /// Gets or sets the map width.
-        /// </summary>
-        [JsonProperty("width")]
-        public int Width { get; set; } = 50;
-
-        /// <summary>
-        /// Gets or sets the world height.
-        /// </summary>
-        [JsonProperty("height")]
-        public int Height { get; set; } = 25;
-
-        /// <summary>
         /// Gets or sets the maximum number of players in a game.
         /// </summary> 
         [JsonProperty("maxNoofPlayers")]
@@ -48,14 +36,6 @@ namespace Cygni.Snake.Client
         public bool FoodEnabled { get; set; } = true;
 
         /// <summary>
-        /// Gets or sets a value indicating if edges wraps around. I.e.,
-        /// hitting an edge does not kill a snake. Instead, it will continue on
-        /// the corresponding edge on the other side.
-        /// </summary>
-        [JsonProperty("edgeWrapsAround")]
-        public bool EdgeWrapsAround { get; set; } = false;
-
-        /// <summary>
         /// Gets or sets a value indicating if tail-eating is enabled. I.e.,
         /// if one snake manages to nibble on the tail of another snake it 
         /// will consume that tail part and the victim snake will loose that
@@ -84,19 +64,5 @@ namespace Cygni.Snake.Client
         /// </summary>
         [JsonProperty("removeFoodLikelihood")]
         public int RemoveFoodLikelihood { get; set; } = 5;
-
-        /// <summary>
-        /// Gets or sets the likelihood (in percent) that a new obstacle will be
-        /// added to the world.
-        /// </summary>
-        [JsonProperty("addObstacleLikelihood")]
-        public int AddObstacleLikelihood { get; set; } = 15;
-
-        /// <summary>
-        /// Gets or sets the likelihood (in percent) that an
-        /// obstacle will be removed from the world
-        /// </summary>
-        [JsonProperty("removeObstacleLikelihood")]
-        public int RemoveObstacleLikelihood { get; set; } = 15;
     }
 }
